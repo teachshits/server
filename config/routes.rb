84a1213e03a2c4d447ca0ba86d57c919f75f924a
Timur Kozmenko <timraell@gmail.com>
2013-02-10 15:05:08 +1100

    Introduce HomeController as root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Server::Application.routes.draw do
+  root :to => 'home#index'
   match 'activities/provoke' => 'activities#provoke'
   match 'activities/dont_provoke' => 'activities#dont_provoke'
 end
